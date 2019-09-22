b31966e48c9067177d6b050ddf5eb9146f9c2a1f
camiloJTG <camilo.emz@gmail.com>
2019-09-22 15:10:03 -0300

    version 0.1

---
+++ TurismoReal/RestApi/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace RestApi.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private Entities db = new Entities();
+
+        //GET api/Usuario
+        public IQueryable<USUARIO> GetClientes()
+        {
+            return db.USUARIO;
+        }
+
+        //POST api/Cliente
+        [ResponseType(typeof(USUARIO))]
+        public IHttpActionResult PostClientes(USUARIO usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                db.USUARIO.Add(usuario);
+                db.SaveChanges();
+                return CreatedAtRoute("DefaultApi", new { id = usuario.USUARIO1 }, usuario);
+            }
+            catch (DbEntityValidationException ex)
+            {
+
+                foreach (var validation in ex.EntityValidationErrors)
+                {
+                    foreach (var va in validation.ValidationErrors)
+                    {
+                        Trace.TraceInformation("propiedad:  {0} DbValidationError: {1}", va.PropertyName,va.ErrorMessage);
+                    }
+                }
+            }
+            return CreatedAtRoute("DefaultApi", new { id = usuario.USUARIO1 }, usuario);
+        }
+    }
+}
---
+++ TurismoReal/RestApi/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RestApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            //ELIMINACIÓN DE DEPENDENCIAS CIRCULARES
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+
+            //WEB API TRABAJA SOLO CON JSON
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+        }
+    }
+}
