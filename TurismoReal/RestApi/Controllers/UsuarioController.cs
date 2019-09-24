d19855aed5c53fb28455010dfa115549474dda83
camiloJTG <camilo.emz@gmail.com>
2019-09-24 11:59:17 -0300

    version 0.5

--- TurismoReal/RestApi/Controllers/UsuarioController.cs
+++ TurismoReal/RestApi/Controllers/UsuarioController.cs
@@ -1,6 +1,8 @@
 ﻿using RestApi.Models.TR;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,12 +16,25 @@ namespace RestApi.Controllers
     {
         private Entities db = new Entities();
 
-        //[HttpGet]
-        //public IEnumerable<USUARIO> GetUsuario()
-        //{
-        //    var list = db.USUARIO.ToList();
-        //    return list;
-        //}
+        [HttpGet]
+        public IEnumerable<Usuario> GetUsuario()
+        {
+            var list = db.USUARIO.ToList();
+            List<Usuario> user = new List<Usuario>();
+
+            foreach (var i in list)
+            {
+                Usuario usuario = new Usuario
+                {
+                    USUARIO1 = i.USUARIO1,
+                    TIPO_USUARIO_CODIGO = i.TIPO_USUARIO_CODIGO,
+                    ESTADO_ID = i.ESTADO_ID,
+                    CONTRASENA = i.CONTRASENA
+                };
+                user.Add(usuario);
+            }
+            return user.ToList();
+        }
 
         [HttpGet]
         public IHttpActionResult GetUsuarioById(string id)
@@ -39,6 +54,92 @@ namespace RestApi.Controllers
                 return Ok(usuario);
             }
             return BadRequest();
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostUsuario(USUARIO usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIO.Add(usuario);
+                db.SaveChanges();
+                return CreatedAtRoute("DefaultApi", new { id = usuario.USUARIO1}, usuario);
+            }
+            return BadRequest(ModelState);
+        }
+
+        //POSIBLE LOGIN
+        //[HttpPost]
+        //public IHttpActionResult PostLogin(string id)
+        //{
+        //    var exitName = db.USUARIO.FirstOrDefault(u=>u.USUARIO1 == id);
+
+        //    if (exitName != null)
+        //    {
+        //        Usuario usuario = new Usuario
+        //        {
+        //            USUARIO1 = exitName.USUARIO1,
+        //            CONTRASENA = exitName.CONTRASENA,
+        //            ESTADO_ID = exitName.ESTADO_ID,
+        //            TIPO_USUARIO_CODIGO = exitName.TIPO_USUARIO_CODIGO
+        //        };
+        //        return Ok(usuario);
+        //    }
+        //    return BadRequest();
+        //}
+
+        [HttpDelete]
+        public IHttpActionResult DeleteUsuario(string id)
+        {
+            var DATA = db.USUARIO.FirstOrDefault(u=>u.USUARIO1 == id);
+
+            if (DATA == null)
+            {
+                return BadRequest();
+            }
+
+            db.USUARIO.Remove(DATA);
+            db.SaveChanges();
+            return Ok(DATA.USUARIO1 + "Eliminado correctamente");
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutUsuario(string id, USUARIO usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.USUARIO1)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        private bool userExists(string id)
+        {
+            return db.USUARIO.Count(e => e.USUARIO1 == id) > 0;
         }
     }
 }
---
+++ TurismoReal/TR.Lógica/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR.Lógica
+{
+    public class Cliente
+    {
+        public string RUN_CLIENTE { get; set; }
+        public string NOMBRE { get; set; }
+        public string APELLIDO_PATERNO { get; set; }
+        public string APELLIDO_MATERNO { get; set; }
+        public string TELEFONO { get; set; }
+        public string EMAIL { get; set; }
+        public Nullable<System.DateTime> FECHA_NACIMIENTO { get; set; }
+        public string DIRECCION { get; set; }
+        public string USUARIO { get; set; }
+        public decimal COMUNA_ID { get; set; }
+        public decimal ESTADO_ID { get; set; }
+        public string NOMBRE_COMUNA { get; set; }
+
+        public virtual Comuna COMUNA { get; set; }
+        //public virtual ESTADO ESTADO { get; set; }
+        public virtual Usuario USUARIO1 { get; set; }
+
+        //public virtual ESTADO ESTADO { get; set; }
+        //public virtual USUARIO USUARIO1 { get; set; }
+    }
+}
